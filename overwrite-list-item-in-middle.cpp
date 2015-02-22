fdeb72c7ccb7f6b14eba4222c336cdf66f16ec4b
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2015-02-22 23:13:15 +0300

    overwrite-list-item-in-middle.cpp

---
+++ overwrite-list-item-in-middle.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cassert>
+#include<memory>
+
+using namespace std;
+
+template <typename T>
+struct Item {
+  const T data;
+  unique_ptr<Item> next;
+  Item(T data, unique_ptr<Item> next = nullptr) : data(data), next(move(next)) {}
+
+  static unique_ptr<Item> create(const char* s) {
+    if (*s) {
+      return unique_ptr<Item>(new Item(*s, create(s+1)));
+    }
+    return nullptr;
+  }
+  void remove() {
+    assert(next);
+    //swap(const_cast<T&>(this->data), const_cast<T&>(next->data));
+    const_cast<T&>(this->data) = move(next->data);
+    next.reset(next->next.release());
+  }
+};
+
+int main() {
+  auto l = Item<char>::create("abcde");
+  assert(l->/*b*/next->/*c*/next->data == 'c');
+  l->/*b*/next->/*c*/next->remove();
+  assert(l->/*b*/next->data == 'b');
+  assert(l->/*b*/next->/*d*/next->data == 'd');
+  return 0;
+}
